5567428f80aa8b98c4c428320f1147ba1f2ec637
Tanner Gould <tgould055@gmail.com>
2024-02-07 13:46:40 -0600

    Successfully makes

---
+++ hw2/hw2/build/CMakeFiles/hw2_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2_test.dir/test/tests.cpp.obj"
+  "CMakeFiles/hw2_test.dir/test/tests.cpp.obj.d"
+  "hw2_test.exe"
+  "hw2_test.exe.manifest"
+  "hw2_test.pdb"
+  "libhw2_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw2_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
